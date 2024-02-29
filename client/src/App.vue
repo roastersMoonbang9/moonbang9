727b5619be226dfdb28509bd6edaa591faede79b
dmg04144 <dmg04144@naver.com>
2024-02-29 17:19:21 +0900

    02/29

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,24 @@
 <template>
+  <HeaderComponent />
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/cart">cart</router-link>
   </nav>
   <router-view/>
+  <FooterComponent />
 </template>
+<script>
+  import HeaderComponent from './components/HeaderComponent.vue'
+  import FooterComponent from './components/FooterComponent.vue'
+
+  export default {
+    components : {
+      HeaderComponent,
+      FooterComponent
+    }
+  }
+</script>
 
 <style>
 #app {
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CartView from '../views/product/CartView.vue'
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: CartView
   }
 ]
 
